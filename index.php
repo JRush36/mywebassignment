b13bc42a6e4b1438d8e8eb8e570c94af7e718dfc
JRush36 <joshrush2003jr@gmail.com>
2026-03-10 09:46:26 +0000

    design

--- index.php
+++ index.php
@@ -5,14 +5,49 @@
 </head>
 <body style="font-family:Arial; background:#f0f0f0; padding:30px; margin:0;">
 
-<h1 style="color:red; text-align:center;">F1 2026 Database</h1>
+<style>
+    table {
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 8px 12px;
+    }
+    nav{
+        padding: 10px 0;
+        background-color: rgb(255, 88, 88);
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    nav ul{
+        padding: 0;
+        margin: 0;
+        list-style: none;
+        display: flex;
+        justify-content: center;}
+     nav a{
+        color: white;
+        text-decoration: none;
+        font-weight: bold;
+        padding: 8px 16px;
+        list-style: none;
+    }
+    nav a:hover{
+        background-color: rgb(188, 56, 56);
+}
+</style>
+<body style="font-family:Arial; background:#f0f0f0; padding:30px;">
 
-<p style="text-align:center; font-size:18px;">
-    <a href="index.php">Home</a> |
-    <a href="drivers.php">Drivers</a> | 
-    <a href="teams.php">Teams</a> | 
-    <a href="tracks.php">Tracks</a> |
+<h1 style="color:red; text-align:center;">Drivers 2026</h1>
+<nav>
+<p>
+    <ul>
+   <li><a href="index.php">Home</a></li> 
+    <li><a href="drivers.php">Drivers</a></li>
+    <li><a href="teams.php">Teams</a></li> 
+    <li><a href="tracks.php">Tracks</a></li>
+</ul>
 </p>
+</nav>
 
 </body>
 </html>
--- teams.php
+++ teams.php
@@ -11,14 +11,49 @@ $teams = $conn->query("SELECT * FROM teams");
 </head>
 <body style="font-family:Arial; background:#f0f0f0; padding:30px;">
 
-<h1 style="color:red; text-align:center;">Teams 2026</h1>
+<style>
+    table {
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 8px 12px;
+    }
+    nav{
+        padding: 10px 0;
+        background-color: rgb(255, 88, 88);
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    nav ul{
+        padding: 0;
+        margin: 0;
+        list-style: none;
+        display: flex;
+        justify-content: center;}
+     nav a{
+        color: white;
+        text-decoration: none;
+        font-weight: bold;
+        padding: 8px 16px;
+        list-style: none;
+    }
+    nav a:hover{
+        background-color: rgb(188, 56, 56);
+}
+</style>
+<body style="font-family:Arial; background:#f0f0f0; padding:30px;">
 
-<p style="text-align:center;">
-    <a href="index.php">Home</a> | 
-    <a href="drivers.php">Drivers</a> |
-    <a href="teams.php">Teams</a> | 
-    <a href="tracks.php">Tracks</a>
+<h1 style="color:red; text-align:center;">Drivers 2026</h1>
+<nav>
+<p>
+    <ul>
+   <li><a href="index.php">Home</a></li> 
+    <li><a href="drivers.php">Drivers</a></li>
+    <li><a href="teams.php">Teams</a></li> 
+    <li><a href="tracks.php">Tracks</a></li>
+</ul>
 </p>
+</nav>
 
 <table border="1" style="margin:0 auto; width:95%; background:white;">
     <tr style="background:#eee;">
